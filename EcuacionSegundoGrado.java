8edda68a266db9f6d4a4dfdb83617452f7476a31
AxelGarciaTello <gata21022000@hotmail.com>
2019-08-21 07:54:26 -0500

    Obtiene la ecuación

---
+++ EcuacionSegundoGrado.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class EcuacionSegundoGrado {
+    
+    private Scanner teclado = new Scanner(System.in);
+    
+    public int[] pedirEcuacion(){
+        int[] terminos = new int[3];
+        System.out.println("Programa pasa solucionar ecuaciones cuadraticas de "
+                + "la forma Ax2+Bx+C=0");
+        System.out.println("Ingrese el coeficiente cuadratico (A):");
+        terminos[0]=Integer.parseInt(teclado.nextLine());
+        System.out.println("Ingrese el coeficiente lineal (B):");
+        terminos[1]=Integer.parseInt(teclado.nextLine());
+        System.out.println("Ingrese el termino independiente (C):");
+        terminos[2]=Integer.parseInt(teclado.nextLine());
+        return terminos;
+    }
+    
+}
