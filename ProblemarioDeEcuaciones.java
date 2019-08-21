29feccfd5f76af39c4ff1ab5d13fe9c93570289e
AxelGarciaTello <gata21022000@hotmail.com>
2019-08-21 07:56:28 -0500

    Función main para ejecutar el programa

---
+++ ProblemarioDeEcuaciones.java
@@ -0,0 +1,8 @@
+public class ProblemarioDeEcuaciones {
+    
+    public static void main(String[] args){
+        Problema problema=new Problema();
+        problema.solucionarProblema();
+    }
+    
+}
