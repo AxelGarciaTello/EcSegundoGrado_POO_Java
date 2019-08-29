84424962fe5c147b3e28390686e98402e62675a2
AxelGarciaTello <gata21022000@hotmail.com>
2019-08-29 10:13:06 -0500

    Ingresar los datos de la Ec.

--- EcuacionSegundoGrado.java
+++ EcuacionSegundoGrado.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class EcuacionSegundoGrado {
     
-    private Scanner teclado = new Scanner(System.in);
+    private Scanner teclado;
     
     public int[] pedirEcuacion(){
+        teclado = new Scanner(System.in);
         int[] terminos = new int[3];
         System.out.println("Programa pasa solucionar ecuaciones cuadraticas de "
                 + "la forma Ax2+Bx+C=0");
